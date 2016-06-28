57757950cf18f6914dfa761b570408e1f804b06a
Caden Albaugh <CadenAlbaugh@gmail.com>
2016-06-28 16:57:35 -0500

    changed the Gruntfile to include responsive images and updated a thumbnail in index.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,57 @@
 module.exports = function(grunt) {
 
 	grunt.initConfig({
+		responsive_images: {
+	      dev: {
+	        options: {
+	          engine: 'im',
+	          sizes: [{
+	            /* Change these */
+	            width: 1287,
+	            suffix: '_large_2x',
+	            quality: 100
+	          }, {
+	            width: 800,
+	            suffix: '_large_1x',
+	            quality: 50
+	          }, {
+	            width: 600,
+	            suffix: '_medium_2x',
+	            quality: 100
+	          }, {
+	            width: 400,
+	            suffix: '_small_2x',
+	            quality: 100
+	          }, {
+	            width: 150,
+	            suffix: '_tiny_2x',
+	            quality: 100
+	          }]
+	        },
+	        files: [{
+	          expand: true,
+	          src: ['*.{gif,jpg,png}'],
+	          cwd: 'views/images_src/',
+	          dest: 'views/images/'
+	        }]
+      	  }
+    	},
+    	/*Clear out the images directory if it exists */
+    	clean: {
+	      dev: {
+	        src: ['images'],
+	      },
+	    },
+
+	    /* Generate the images directory if it is missing */
+	    mkdir: {
+	      dev: {
+	        options: {
+	          create: ['images']
+	        },
+	      },
+	    },
+
 		pkg: grunt.file.readJSON('package.json'),
 
 		uglify: {
@@ -12,7 +63,7 @@ module.exports = function(grunt) {
 			},
 		},
 	});
-
+	grunt.loadNpmTasks('grunt-responsive-images');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 	grunt.registerTask('default', ['uglify']);
